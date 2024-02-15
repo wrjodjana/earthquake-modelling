6b3b7d7d44fccd081151f85db4c30c9c9a833f68
Jonathan Bernard Widjajakusuma <jbw7@illinois.edu>
2024-02-15 04:12:13 -0600

    cleaned dataframe

--- magnitude.py
+++ magnitude.py
@@ -8,8 +8,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.svm import SVC
 from sklearn.model_selection import GridSearchCV
-from sklearn.metrics import classification_report
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import classification_report, accuracy_score
 from imblearn.combine import SMOTEENN
 
 print(df)
