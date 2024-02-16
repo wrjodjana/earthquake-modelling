8c327e27623dea9f9f18bf8dd4adc1195dc856f0
wrjodjana <141975732+wrjodjana@users.noreply.github.com>
2024-02-16 00:32:56 -0600

    changed boundaries dataset (in progress)

--- mapping.py
+++ mapping.py
@@ -15,12 +15,20 @@ from sklearn.metrics import accuracy_score
 
 from imblearn.combine import SMOTEENN
 
-import contextily as ctx
-from shapely.geometry import Point
 
+# earthquake data
 df = pd.read_csv('earthquake-data.csv')
 
-gdf = gpd.read_file("datasets/batas_negara/Indonesia.shp")
+# boundaries data
+boundaries = gpd.read_file('datasets/indonesia_boundaries.geojson')
+print(boundaries.head())
+print(boundaries.geometry)
+print(boundaries.geometry.iloc[0])
+
+boundaries.plot()
+plt.show()
+
+
 
 # added translations
 translations = {
@@ -37,7 +45,6 @@ df.rename(columns = translations, inplace = True)
 # remove unuseful columns
 df_drop = df.drop(columns=['strike1', 'dip1', 'rake1', 'strike2', 'rake2'])
 df = df_drop
-print(df)
 
 # dropping NaN values
 df.dropna(inplace=True)
@@ -45,36 +52,6 @@ df.dropna(inplace=True)
 # convert dates to datetime type
 df['Date'] = pd.to_datetime(df['Date'])
 
-# time series of earthquakes over the years
-plt.figure(figsize =(12,8))
-time_series = sns.lineplot(x=df['Date'].dt.year,y="Magnitude",data=df,color="#ffa600")
-time_series.set_title("Time Series Of Earthquakes in Indonesia", color="#58508d")
-time_series.set_ylabel("Magnitude", color="#58508d")
-time_series.set_xlabel("Date", color="#58508d")
-plt.show()
 
-# magnitude graphs
-plt.figure(figsize=(12,9))
-mean_magnitude = df["Magnitude"].mean()
-magnitude_plot = sns.distplot(df["Magnitude"].values, color ="#ffa600")
-magnitude_plot.set_title("Magnitudes of Earthquakes in Indonesia")
-magnitude_plot.set_ylabel("Depth", color="#58508d")
-magnitude_plot.set_xlabel("Magnitude", color="#58508d")
-plt.show()
 
-# geospatial visualization
-
-# convert to geodataframe and change crs
-gdf_earthquakes = gpd.GeoDataFrame(df, geometry=gpd.points_from_xy(df["Longitude"], df["Latitude"]))
-gdf_earthquakes.crs = "EPSG:4326"
-gdf_earthquakes = gdf_earthquakes.to_crs(epsg=3857)
-
-# plot indonesian boundaries
-fig, ax = plt.subplots(figsize=(15, 10))
-gdf.to_crs(epsg=3857).plot(ax=ax, edgecolor='black', color='none')
-ctx.add_basemap(ax, source=ctx.providers.CartoDB.Positron)
-gdf_earthquakes.plot(ax=ax, marker='o', color='red', markersize=5, alpha=0.5)
-ax.set_axis_off()
-plt.title("Earthquake Magnitudes with Indonesian Boundaries")
-plt.show()
 
